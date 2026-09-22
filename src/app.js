b9edc10d7932d3b81c95f9b38bd024792ac0f360
ArturH16 <holandaartur163@gmail.com>
2026-09-22 15:00:14 -0300

    criacao da estrutura basica rota cadastro

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
 const express = require("express")
-const app = express.Router()
+const app = express()
+const PORT = 3000
+const {engine} = require("express-handlebars")
+const path = require("path")
+const cadastroRouter = require("./routes/cadastro")
+//Configurações Padrões
+    //Template Engine
+    app.engine("handlebars",engine())
+    app.set("view engine","handlebars")
+    app.set("views",path.join(__dirname,"views"))
+    
+
+//Rotas
+    app.use("/cadastro",cadastroRouter)
+
+
+
+    app.listen(PORT,()=> {
+        console.log(`Servidor rodando na porta ${PORT}!`)
+    })
