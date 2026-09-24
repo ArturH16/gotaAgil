030d4e85e25214dc0c3151270ec65ed224f8347b
ArturH16 <holandaartur163@gmail.com>
2026-09-24 17:12:21 -0300

    Adicao cadastros hemocentro doador

--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -2,9 +2,23 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/",(req,res)=> {
-    res.render("./cadastro", {
+    res.render("cadastro", {
         layout: "auth",
         title: "Cadastro"
+    })
+})
+
+router.get("/doador",(req,res)=> {
+    res.render("cadastro_doador", {
+        layout: "auth",
+        title: "Cadastro Doador"
+    })
+})
+
+router.get("/hemocentro",(req,res)=> {
+    res.render("cadastro_hemocentro", {
+        layout: "auth",
+        title: "Cadastro Hemocentro"
     })
 })
 
